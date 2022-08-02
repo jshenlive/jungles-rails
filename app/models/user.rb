9941188d675c51ae38820498d4a9256ac01651b9
Jesse Shen <jly.shen@gmail.com>
2022-08-01 21:57:22 -0700

    add authenticates with edge cases

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ActiveRecord::Base
   validates :password, length: {minimum:4}
   
   def self.authenticate_with_credentials(email,password)
-    user = User.find_by_email(email.strip)
+    user = User.find_by_email(email.strip.downcase)
     if user && user.authenticate(password)
       true
     else 
