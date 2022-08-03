1027d7e1f00eb6dd6b4eaff35e2542d35206f68c
Jesse Shen <jly.shen@gmail.com>
2022-08-02 21:20:45 -0700

    test add to cart

---
+++ cypress/integration/add_to_cart.spec.js
@@ -0,0 +1,20 @@
+describe('Add to cart', () => {
+
+  // it('displays home page', () => {
+  //   cy.visit('http://localhost:3000/')
+  // })
+  
+  beforeEach(() => {
+  
+    // Cypress starts out with a blank slate for each test
+    // so we must tell it to visit our website with the `cy.visit()` command.
+    // Since we want to visit the same URL at the start of all our tests,
+    // we include it in our beforeEach function so that it runs before each test
+    cy.visit('http://localhost:3030/')
+  })
+  
+  it("Click Add to Cart", () => {
+    cy.contains('Add').click({force: true})
+    cy.get('.end-0').should('contains.text',1)
+  });
+  })
